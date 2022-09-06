bc41f1962a32fbec4206b4b8842df898aa9d489a
Aalb27 <a.baloul@hotmail.com>
2022-09-06 10:00:07 +0300

    Opacity says updating takes longer than 5 seconds

---
+++ CW1-1/CW1-1/CW1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  CW1_1App.swift
+//  CW1-1
+//
+//  Created by Abdulaziz Albalool on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CW1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CW1-1/CW1-1/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  CW1-1
+//
+//  Created by Abdulaziz Albalool on 06/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+        ZStack{
+            Color.blue
+            VStack{
+                Image(systemName: "magnifyingglass")
+                    
+                Text("Hello, My name is Abdulaziz")
+            Text("I am 29 years old")
+                Text("I am learning SwiftUI now")
+    HStack{
+       
+        Image(systemName: "heart.fill")
+        Spacer()
+        Image(systemName: "gear")
+        Spacer()
+    Image(systemName:"trash.fill")}
+
+        
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+}
