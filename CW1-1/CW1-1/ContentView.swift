0f9593bf46c70651eeecf0071f6aa6b1b9d9e579
Aalb27 <a.baloul@hotmail.com>
2022-09-06 10:07:16 +0300

    completed

--- CW1-1/CW1-1/ContentView.swift
+++ CW1-1/CW1-1/ContentView.swift
@@ -12,26 +12,25 @@ struct ContentView: View {
         VStack{
         ZStack{
             Color.blue
+                .opacity(0.5)
             VStack{
-                Image(systemName: "magnifyingglass")
                     
                 Text("Hello, My name is Abdulaziz")
             Text("I am 29 years old")
                 Text("I am learning SwiftUI now")
     HStack{
-       
         Image(systemName: "heart.fill")
         Spacer()
         Image(systemName: "gear")
         Spacer()
     Image(systemName:"trash.fill")}
-
+   
         
             }
         }
-    }
-}
-
+            .ignoresSafeArea()}
+        }
+        
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ cw1-2/cw1-2/cw1_2App.swift
@@ -0,0 +1,17 @@
+//
+//  cw1_2App.swift
+//  cw1-2
+//
+//  Created by Abdulaziz Albalool on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw1_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
